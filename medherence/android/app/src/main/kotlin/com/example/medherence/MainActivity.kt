784a22898657094656e16e00cecdbb5f3b2142e9
SteveTayor <ayomidearose75@gmail.com>
2024-03-12 13:34:47 +0100

    iniit

---
+++ medherence/android/app/src/main/kotlin/com/example/medherence/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.medherence
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ medherence/lib/features/auth_features/views/login_view.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Sign In',
+            style: TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          centerTitle: true,
+          toolbarHeight: 100,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(
+            left: 25.0,
+            right: 25,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Welcome',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Enter  your login details to access the app',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
