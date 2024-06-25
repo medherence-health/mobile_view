457fce8ad2866395ac36179b4aee0b286a1ee67b
SteveTayor <ayomidearose75@gmail.com>
2024-06-25 13:48:33 -0700

    fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:medherence/core/service/biometric_service.dart';
+import 'package:mockito/mockito.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:medherence/main.dart';
 import 'package:medherence/features/splashscreen/splashscreen.dart';
