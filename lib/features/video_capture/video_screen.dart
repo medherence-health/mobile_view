ced89bd5cf0c2a8837797f18dd34a95b2cda3e1e
Abraham <enarebebenatthan@gmail.com>
2025-01-27 08:19:48 -0800

    Video recorder orientation

--- lib/features/video_capture/video_screen.dart
+++ lib/features/video_capture/video_screen.dart
@@ -1,10 +1,13 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoCaptureScreen extends StatefulWidget {
   const VideoCaptureScreen({Key? key}) : super(key: key);
@@ -19,17 +22,20 @@ class _VideoCaptureScreenState extends State<VideoCaptureScreen> {
   bool _isRecording = false;
   bool _isCameraInitialized = false;
   bool _hasPermission = false;
-  int _countdown = 30; // Countdown starting at 30 seconds
-  late Timer _timer; // Timer for countdown
+  int _countdown = 30;
+  late Timer _timer;
+  late VideoPlayerController _videoPlayerController;
+  String _videoPath = '';
+  bool _isPlaying = true;
 
   @override
   void initState() {
     super.initState();
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     _checkPermissions();
   }
 
   Future<void> _checkPermissions() async {
-    // Check and request camera permission
     if (await Permission.camera.isGranted) {
       _hasPermission = true;
       _initializeCamera();
@@ -39,61 +45,6 @@ class _VideoCaptureScreenState extends State<VideoCaptureScreen> {
     }
   }
 
-  Future<void> _requestPermission() async {
-    final status = await Permission.camera.status;
-
-    if (status.isGranted) {
-      _hasPermission = true;
-      _initializeCamera();
-    } else if (status.isDenied) {
-      final newStatus = await Permission.camera.request();
-      if (newStatus.isGranted) {
-        _hasPermission = true;
-        _initializeCamera();
-      } else {
-        _hasPermission = false;
-        WidgetsBinding.instance.addPostFrameCallback((_) {
-          if (mounted) {
-            _showPermissionDeniedDialog();
-          }
-        });
-      }
-    } else if (status.isPermanentlyDenied) {
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        if (mounted) {
-          _showPermissionDeniedDialog();
-        }
-      });
-    }
-    setState(() {});
-  }
-
-  void _showPermissionDeniedDialog() {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text("Camera Permission Required"),
-        content: const Text(
-            "Camera permission is required to use this feature. Please enable it in the app settings."),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: const Text("Cancel"),
-          ),
-          TextButton(
-            onPressed: () async {
-              await openAppSettings();
-              Navigator.of(context).pop();
-            },
-            child: const Text("Open Settings"),
-          ),
-        ],
-      ),
-    );
-  }
-
   Future<void> _initializeCamera() async {
     try {
       _cameras = await availableCameras();
@@ -101,17 +52,17 @@ class _VideoCaptureScreenState extends State<VideoCaptureScreen> {
         _cameras.first,
         ResolutionPreset.high,
       );
-
       await _cameraController!.initialize();
       setState(() {
         _isCameraInitialized = true;
       });
     } catch (e) {
-      print("Error initializing camera: $e");
+      debugPrint("Error initializing camera: $e");
     }
   }
 
   Future<void> _startVideoRecording() async {
+    _countdown = 30;
     if (_cameraController != null &&
         _cameraController!.value.isInitialized &&
         !_isRecording) {
@@ -121,7 +72,7 @@ class _VideoCaptureScreenState extends State<VideoCaptureScreen> {
         });
 
         final directory = await getTemporaryDirectory();
-        final videoPath = path.join(directory.path, '${DateTime.now()}.mp4');
+        _videoPath = path.join(directory.path, '${DateTime.now()}.mp4');
 
         await _cameraController!.startVideoRecording();
         _startCountdown();
@@ -133,15 +84,10 @@ class _VideoCaptureScreenState extends State<VideoCaptureScreen> {
           _isRecording = false;
         });
 
-        file.saveTo(videoPath);
-        print("Video saved to $videoPath");
-
-        // Reset countdown after recording
-        setState(() {
-          _countdown = 30;
-        });
+        file.saveTo(_videoPath);
+        _initializeVideoPreview();
       } catch (e) {
-        print("Error during video recording: $e");
+        debugPrint("Error during video recording: $e");
         setState(() {
           _isRecording = false;
         });
@@ -153,9 +99,7 @@ class _VideoCaptureScreenState extends State<VideoCaptureScreen> {
     const oneSec = Duration(seconds: 1);
     _timer = Timer.periodic(oneSec, (Timer timer) {
       if (_countdown == 0) {
-        setState(() {
-          _isRecording = false;
-        });
+        _stopRecording();
         timer.cancel();
       } else {
         setState(() {
@@ -165,10 +109,37 @@ class _VideoCaptureScreenState extends State<VideoCaptureScreen> {
     });
   }
 
+  void _initializeVideoPreview() {
+    _videoPlayerController = VideoPlayerController.file(File(_videoPath))
+      ..initialize().then((_) {
+        setState(() {});
+        _videoPlayerController.play();
+      });
+  }
+
+  void _stopRecording() async {
+    if (_cameraController != null &&
+        _cameraController!.value.isRecordingVideo) {
+      final file = await _cameraController!.stopVideoRecording();
+
+      setState(() {
+        _isRecording = false;
+      });
+
+      file.saveTo(_videoPath);
+      _initializeVideoPreview();
+      _timer.cancel();
+    }
+  }
+
   @override
   void dispose() {
     _cameraController?.dispose();
     _timer.cancel();
+    if (_videoPlayerController.value.isInitialized) {
+      _videoPlayerController.dispose();
+    }
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     super.dispose();
   }
 
@@ -178,24 +149,57 @@ class _VideoCaptureScreenState extends State<VideoCaptureScreen> {
       appBar: AppBar(title: const Text("Capture Video")),
       body: _hasPermission
           ? _isCameraInitialized
-              ? Column(
+              ? Stack(
                   children: [
-                    AspectRatio(
-                      aspectRatio: _cameraController!.value.aspectRatio,
-                      child: CameraPreview(_cameraController!),
+                    // Camera preview
+                    Center(
+                      child: Transform.scale(
+                        scale: 1 /
+                            (_cameraController!.value.aspectRatio /
+                                MediaQuery.of(context).size.aspectRatio),
+                        child: AspectRatio(
+                          aspectRatio: _cameraController!.value.aspectRatio,
+                          child: Transform.rotate(
+                            angle: 90 * (3.141592653589793 / 180),
+                            child: CameraPreview(_cameraController!),
+                          ),
+                        ),
+                      ),
                     ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: _isRecording ? null : _startVideoRecording,
-                      child: Text(_isRecording
-                          ? "Recording... ($_countdown)"
-                          : "Start Recording"),
-                    ),
+                    if (_isRecording)
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                "Recording... ($_countdown)",
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                              ElevatedButton(
+                                onPressed: _stopRecording,
+                                child: const Text("Stop Recording"),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    else
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: ElevatedButton(
+                            onPressed: _startVideoRecording,
+                            child: const Text("Start Recording"),
+                          ),
+                        ),
+                      ),
                   ],
                 )
-              : const Center(
-                  child: CircularProgressIndicator(),
-                )
+              : const Center(child: CircularProgressIndicator())
           : Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -206,7 +210,7 @@ class _VideoCaptureScreenState extends State<VideoCaptureScreen> {
                   ),
                   const SizedBox(height: 20),
                   ElevatedButton(
-                    onPressed: _requestPermission,
+                    onPressed: _checkPermissions,
                     child: const Text("Enable Camera Permission"),
                   ),
                 ],
