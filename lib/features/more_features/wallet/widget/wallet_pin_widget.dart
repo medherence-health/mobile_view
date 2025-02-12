f9ee86808afa3b1f6ff394787eaad29403ffc0d8
Abraham <enarebebenatthan@gmail.com>
2025-02-12 03:48:44 -0800

    wallet pin validation

--- lib/features/more_features/wallet/widget/wallet_pin_widget.dart
+++ lib/features/more_features/wallet/widget/wallet_pin_widget.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:medherence/core/utils/color_utils.dart';
-
-import '../../../../core/utils/size_manager.dart';
+import 'package:medherence/features/profile/view_model/profile_view_model.dart';
+import 'package:provider/provider.dart';
 
 class WalletPinWidget extends StatefulWidget {
   const WalletPinWidget({Key? key}) : super(key: key);
@@ -17,9 +16,6 @@ class _WalletPinWidgetState extends State<WalletPinWidget> {
       List.generate(4, (_) => TextEditingController());
   final FocusNode _focusNode = FocusNode();
 
-  bool _allFieldsFilled = false; // Flag to track if all fields have input
-  String _validationMessage = ""; // String to store validation message
-
   @override
   void dispose() {
     for (final controller in _controllers) {
@@ -29,110 +25,129 @@ class _WalletPinWidgetState extends State<WalletPinWidget> {
     super.dispose();
   }
 
+  Future<String> _fetchCurrentPin(BuildContext context) async {
+    var res = await context.watch<ProfileViewModel>().getSecurity();
+    return res.security?.pin ?? "aaaa";
+  }
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoAlertDialog(
-      content: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SizedBox(
-          height: 150,
-          child: Column(
-            children: [
-              const Text(
-                'Enter your Wallet Pin to enable fingerprint',
+    return FutureBuilder<String>(
+      future: _fetchCurrentPin(context),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text("Error: ${snapshot.error}"));
+        } else if (!snapshot.hasData) {
+          return const Center(child: Text("Failed to load PIN"));
+        }
+
+        String currentPin = snapshot.data!;
+
+        return CupertinoAlertDialog(
+          content: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: 200,
+              child: StatefulBuilder(
+                builder: (context, setStateLocal) {
+                  String validationMessage = "";
+
+                  return Column(
+                    children: [
+                      const Text('Enter your Wallet Pin to enable fingerprint'),
+                      const SizedBox(height: 16),
+                      Container(
+                        width: 200,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: List.generate(
+                            4,
+                            (index) => Flexible(
+                              child: _buildPinTextField(
+                                index,
+                                currentPin,
+                                setStateLocal,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      ValueListenableBuilder<String>(
+                        valueListenable: _errorNotifier,
+                        builder: (context, value, child) {
+                          if (value.isNotEmpty) {
+                            return Text(
+                              value,
+                              style: const TextStyle(color: Colors.red),
+                            );
+                          }
+                          return const SizedBox.shrink();
+                        },
+                      ),
+                    ],
+                  );
+                },
               ),
-              const SizedBox(height: 16),
-              Expanded(
-                child: Container(
-                  width: SizeMg.width(200),
-                  height: SizeMg.height(60),
-                  padding: const EdgeInsets.all(0),
-                  decoration: BoxDecoration(
-                    color: AppColors.white,
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: List.generate(
-                      4,
-                      (index) => Flexible(child: _buildPinTextField(index)),
-                    ),
-                  ),
-                ),
-              ),
-              if (_validationMessage.isNotEmpty)
-                Text(
-                  _validationMessage,
-                  style: TextStyle(color: Colors.red),
-                ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 
-  Widget _buildPinTextField(int index) {
-    return Container(
-      width: SizeMg.width(28),
-      height: SizeMg.height(28),
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        color: _controllers[index].text.isNotEmpty
-            ? AppColors.historyBackground
-            : Colors.grey,
-        shape: BoxShape.circle,
-      ),
-      child: CupertinoTextField(
-        controller: _controllers[index],
-        focusNode: index == 0 ? _focusNode : null,
-        obscureText: true,
-        textAlign: TextAlign.center,
-        keyboardType: TextInputType.number,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.transparent),
-        ),
-        inputFormatters: [
-          LengthLimitingTextInputFormatter(1),
-          FilteringTextInputFormatter.digitsOnly
-        ],
-        onChanged: (value) {
-          // Check if all fields are filled
-          _allFieldsFilled =
-              _controllers.every((controller) => controller.text.isNotEmpty);
+  final ValueNotifier<String> _errorNotifier = ValueNotifier<String>("");
 
-          // Update validation message
-          _validationMessage = "";
-          if (index == 3 && !_allFieldsFilled) {
-            _validationMessage = "All fields are required to have an input";
-          }
-
-          if (value.isEmpty) {
-            // If the field is empty, move to the previous field
-            if (index > 0) {
-              FocusScope.of(context).previousFocus();
-            }
-          } else if (value.length == 1 || index < 3) {
-            // If a digit is entered, move to the next field
-            FocusScope.of(context).nextFocus();
-          }
-          if (index == 3 && _allFieldsFilled) {
-            // Last digit entered and all fields filled, perform verification
-            _verifyPinAndCloseDialog();
-          }
-          setState(() {}); // Rebuild widget to show updated validation message
-        },
+  Widget _buildPinTextField(
+      int index, String currentPin, Function setStateLocal) {
+    return CupertinoTextField(
+      controller: _controllers[index],
+      focusNode: index == 0 ? _focusNode : null,
+      obscureText: true,
+      textAlign: TextAlign.center,
+      keyboardType: TextInputType.number,
+      decoration: const BoxDecoration(
+        border: Border.fromBorderSide(BorderSide.none),
       ),
+      inputFormatters: [
+        LengthLimitingTextInputFormatter(1),
+        FilteringTextInputFormatter.digitsOnly,
+      ],
+      onChanged: (value) {
+        bool allFieldsFilled = _controllers.every((c) => c.text.isNotEmpty);
+
+        if (value.isEmpty) {
+          if (index > 0) FocusScope.of(context).previousFocus();
+        } else if (value.length == 1 || index < 3) {
+          FocusScope.of(context).nextFocus();
+        }
+
+        if (index == 3 && allFieldsFilled) {
+          _verifyPinAndCloseDialog(currentPin);
+        }
+
+        setStateLocal(() {});
+      },
     );
   }
 
-  void _verifyPinAndCloseDialog() {
-    final enteredPin = _controllers.map((controller) => controller.text).join();
-    // Perform pin verification here with enteredPin
-    print('Entered PIN: $enteredPin');
-
-    if (_allFieldsFilled) {
-      Navigator.pop(context, true); // Close the dialog and return true
+  void _verifyPinAndCloseDialog(String currentPin) {
+    final enteredPin = _controllers.map((c) => c.text).join();
+    if (enteredPin == currentPin) {
+      Navigator.pop(context, true);
+    } else {
+      _errorNotifier.value = "Incorrect PIN. Try again.";
+      for (var controller in _controllers) {
+        controller.clear();
+      }
+      _focusNode.requestFocus(); // Reset focus to the first field
     }
   }
 }
